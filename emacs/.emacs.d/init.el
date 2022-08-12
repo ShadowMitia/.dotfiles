d9f30b1ada9924263611612aea433fe285408ecb
Dimitri Belopopsky <dimitri@belopopsky.com>
2022-08-12 22:04:24 +0200

    Update emacs config

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,5 +1,12 @@
+;;; init.el --- Emacs entry point  -*- lexical-binding: t; -*-
+
+(setq-default vc-follow-symlinks t)
+
 ;; (debug-on-entry 'package-initialize)
 (require 'org)
-(org-babel-load-file
- (expand-file-name "configuration/emacs_configuration.org"
-                   user-emacs-directory))
+
+(setq emacs_config_file (concat user-emacs-directory "configuration/emacs_configuration.org"))
+
+(org-babel-tangle-file emacs_config_file)
+
+(org-babel-load-file (expand-file-name emacs_config_file user-emacs-directory))
