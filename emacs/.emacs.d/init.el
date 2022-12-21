d5ee851cb512bf5e81bcb67578f8e191b9deb714
Dimitri Belopopsky <dimitri@belopopsky.com>
2022-12-21 19:53:20 +0100

    Add init.el

---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,19 @@
+;;; init.el --- Emacs entry point
+;;; -*- lexical-binding: t; -*-
+
+;;; Follow links always (so it can follow dotfiles files)
+(setq-default vc-follow-symlinks t)
+
+;; (debug-on-entry 'package-initialize)
+;;; We need org-babel-tangle-file available
+(require 'org)
+
+(setq emacs_config_file (concat user-emacs-directory "../.dotfiles/emacs/.emacs.d/configuration/emacs_configuration.org"))
+(setq emacs_config_file_el (concat user-emacs-directory "../.dotfiles/emacs/.emacs.d/configuration/emacs_configuration.el"))
+
+(when (file-newer-than-file-p emacs_config_file emacs_config_file_el)
+  (org-babel-tangle-file emacs_config_file)
+  )
+
+;;; (org-babel-load-file (expand-file-name emacs_config_file user-emacs-directory))
+(load emacs_config_file_el)
