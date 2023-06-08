e9d464c90eaaffc06f4c3c40e1dae5e0d82180ff
Dimitri Belopopsky <dimitri.belopopsky@parrot.com>
2023-06-08 16:32:09 +0200

    MEGA UPDATE
    Change-Id: I56f76e87a4cf995975b7076a2b0682fcece771e5

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -18,3 +18,15 @@
 
 ;;; (org-babel-load-file (expand-file-name emacs_config_file user-emacs-directory))
 (load emacs_config_file_el)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(eldoc-box-body ((t (:background "#2E3440")))))
